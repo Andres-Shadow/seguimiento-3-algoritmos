e963ed42b00ba6cc2953fbda651cda503cb224f8
luisfer010 <luisf.arenasr@uqvirtual.edu.co>
2023-10-24 16:48:50 -0500

    naivLoop3

--- algoritmos/naivLoopUnrollingThree.go
+++ algoritmos/naivLoopUnrollingThree.go
@@ -4,32 +4,44 @@ import (
 	"fmt"
 )
 
-func NaivLoopUnrollingTwo(A, B, Result [][]float64, N, P, M int) {
-	if P%2 == 0 {
+func NaivLoopUnrollingThree(A, B, Result [][]float64, N, P, M int) {
+	if P%3 == 0 {
 		for i := 0; i < N; i++ {
 			for j := 0; j < M; j++ {
 				var aux float64
-				for k := 0; k < P; k += 2 {
-					aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j]
+				for k := 0; k < P; k += 3 {
+					aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j] + A[i][k+2]*B[k+2][j]
 				}
 				Result[i][j] = aux
 			}
 		}
-	} else {
+	} else if P%3 == 1 {
 		var PP int = P - 1
 		for i := 0; i < N; i++ {
 			for j := 0; j < M; j++ {
 				var aux float64
-				for k := 0; k < PP; k += 2 {
-					aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j]
+				for k := 0; k < PP; k += 3 {
+					aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j] + A[i][k+2]*B[k+2][j]
 				}
 				Result[i][j] = aux + A[i][PP]*B[PP][j]
+			}
+		}
+	} else {
+		var PP int = P - 2
+		var PPP int = P - 1
+		for i := 0; i < N; i++ {
+			for j := 0; j < M; j++ {
+				var aux float64
+				for k := 0; k < PP; k += 3 {
+					aux += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j] + A[i][k+2]*B[k+2][j]
+				}
+				Result[i][j] = aux + A[i][PP]*B[PP][j] + A[i][PPP]*B[PPP][j]
 			}
 		}
 	}
 }
 
-func LlamarNaivLoopUnrollingTwo() {
+func LlamarNaivLoopUnrollingThree() {
 	var A, B, Result [][]float64 = make([][]float64, 3), make([][]float64, 3), make([][]float64, 3)
 	for i := 0; i < 3; i++ {
 		A[i] = make([]float64, 3)
@@ -45,8 +57,8 @@ func LlamarNaivLoopUnrollingTwo() {
 		}
 	}
 
-	// Compute the matrix product using the NaivLoopUnrollingTwo algorithm
-	NaivLoopUnrollingTwo(A, B, Result, 3, 3, 3)
+	// Compute the matrix product using the NaivLoopUnrollingThree algorithm
+	NaivLoopUnrollingThree(A, B, Result, 3, 3, 3)
 
 	// Print the result matrix
 	fmt.Println("Result:")

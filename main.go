4caf8159bb1fb847639ce253f01ab0049433942a
Andres-Shadow <andresm.dussanb@uqvirtual.edu.co>
2023-10-24 16:58:02 -0500

    matrices

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "seg_3_algoritmos/algoritmos"
 
 func main() {
+	//rows, cols := 1024, 1024
+	//matrix := generateRandomMatrix(rows, cols)
+	//saveMatrixToFile(matrix, "datos.dat")
+	//m := herramientas.GenerateRandomMatrix(1024, 2014)
+	//herramientas.SaveMatrixToFile(m, "datos2.dat")
 	algoritmos.LlamarNaivStandart()
 }

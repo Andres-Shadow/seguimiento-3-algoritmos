0e8b446dff35983fda2801e773bb1252f67a1bbf
luisfer010 <luisf.arenasr@uqvirtual.edu.co>
2023-10-25 17:39:45 -0500

    StrassenNaiv correción

--- algoritmos/strassenNaiv.go
+++ algoritmos/strassenNaiv.go
@@ -54,9 +54,114 @@ func StrassenNaiv(A [][]float64, B [][]float64, Result [][]float64, N int, P int
 	}
 }
 
-func StrassenNaivStep(A [][]float64, B [][]float64, Result [][]float64, Size int, m int) {
-	// implementation of Strassen's algorithm
-	// ...
+func StrassenNaivStep(A, B, Result [][]float64, N, m int) {
+	if N%2 == 0 && N > m {
+		NewSize := N / 2
+
+		A11 := make([][]float64, NewSize)
+		A12 := make([][]float64, NewSize)
+		A21 := make([][]float64, NewSize)
+		A22 := make([][]float64, NewSize)
+		B11 := make([][]float64, NewSize)
+		B12 := make([][]float64, NewSize)
+		B21 := make([][]float64, NewSize)
+		B22 := make([][]float64, NewSize)
+		ResultPart11 := make([][]float64, NewSize)
+		ResultPart12 := make([][]float64, NewSize)
+		ResultPart21 := make([][]float64, NewSize)
+		ResultPart22 := make([][]float64, NewSize)
+		Helper1 := make([][]float64, NewSize)
+		Helper2 := make([][]float64, NewSize)
+		Aux1 := make([][]float64, NewSize)
+		Aux2 := make([][]float64, NewSize)
+		Aux3 := make([][]float64, NewSize)
+		Aux4 := make([][]float64, NewSize)
+		Aux5 := make([][]float64, NewSize)
+		Aux6 := make([][]float64, NewSize)
+		Aux7 := make([][]float64, NewSize)
+
+		for i := 0; i < NewSize; i++ {
+			A11[i] = make([]float64, NewSize)
+			A12[i] = make([]float64, NewSize)
+			A21[i] = make([]float64, NewSize)
+			A22[i] = make([]float64, NewSize)
+			B11[i] = make([]float64, NewSize)
+			B12[i] = make([]float64, NewSize)
+			B21[i] = make([]float64, NewSize)
+			B22[i] = make([]float64, NewSize)
+			ResultPart11[i] = make([]float64, NewSize)
+			ResultPart12[i] = make([]float64, NewSize)
+			ResultPart21[i] = make([]float64, NewSize)
+			ResultPart22[i] = make([]float64, NewSize)
+			Helper1[i] = make([]float64, NewSize)
+			Helper2[i] = make([]float64, NewSize)
+			Aux1[i] = make([]float64, NewSize)
+			Aux2[i] = make([]float64, NewSize)
+			Aux3[i] = make([]float64, NewSize)
+			Aux4[i] = make([]float64, NewSize)
+			Aux5[i] = make([]float64, NewSize)
+			Aux6[i] = make([]float64, NewSize)
+			Aux7[i] = make([]float64, NewSize)
+		}
+
+		for i := 0; i < NewSize; i++ {
+			for j := 0; j < NewSize; j++ {
+				A11[i][j] = A[i][j]
+				A12[i][j] = A[i][NewSize+j]
+				A21[i][j] = A[NewSize+i][j]
+				A22[i][j] = A[NewSize+i][NewSize+j]
+				B11[i][j] = B[i][j]
+				B12[i][j] = B[i][NewSize+j]
+				B21[i][j] = B[NewSize+i][j]
+				B22[i][j] = B[NewSize+i][NewSize+j]
+			}
+		}
+
+		// Computing the seven auxiliary variables
+		Plus(A11, A22, Helper1, NewSize, NewSize)
+		Plus(B11, B22, Helper2, NewSize, NewSize)
+		StrassenNaivStep(Helper1, Helper2, Aux1, NewSize, m)
+		Plus(A21, A22, Helper1, NewSize, NewSize)
+		StrassenNaivStep(Helper1, B11, Aux2, NewSize, m)
+		Minus(B12, B22, Helper1, NewSize, NewSize)
+		StrassenNaivStep(A11, Helper1, Aux3, NewSize, m)
+		Minus(B21, B11, Helper1, NewSize, NewSize)
+		StrassenNaivStep(A22, Helper1, Aux4, NewSize, m)
+		Plus(A11, A12, Helper1, NewSize, NewSize)
+		StrassenNaivStep(Helper1, B22, Aux5, NewSize, m)
+		Minus(A21, A11, Helper1, NewSize, NewSize)
+		Plus(B11, B12, Helper2, NewSize, NewSize)
+		StrassenNaivStep(Helper1, Helper2, Aux6, NewSize, m)
+		Minus(A12, A22, Helper1, NewSize, NewSize)
+		Plus(B21, B22, Helper2, NewSize, NewSize)
+		StrassenNaivStep(Helper1, Helper2, Aux7, NewSize, m)
+
+		// Computing the four parts of the result
+		Plus(Aux1, Aux4, ResultPart11, NewSize, NewSize)
+		Minus(ResultPart11, Aux5, ResultPart11, NewSize, NewSize)
+		Plus(ResultPart11, Aux7, ResultPart11, NewSize, NewSize)
+		Plus(Aux3, Aux5, ResultPart12, NewSize, NewSize)
+		Plus(Aux2, Aux4, ResultPart21, NewSize, NewSize)
+		Plus(Aux1, Aux3, ResultPart22, NewSize, NewSize)
+		Minus(ResultPart22, Aux2, ResultPart22, NewSize, NewSize)
+		Plus(ResultPart22, Aux6, ResultPart22, NewSize, NewSize)
+
+		// Store results in the "result matrix"
+		for i := 0; i < NewSize; i++ {
+			for j := 0; j < NewSize; j++ {
+				Result[i][j] = ResultPart11[i][j]
+				Result[i][NewSize+j] = ResultPart12[i][j]
+				Result[NewSize+i][j] = ResultPart21[i][j]
+				Result[NewSize+i][NewSize+j] = ResultPart22[i][j]
+			}
+		}
+
+		// Free helper variables
+
+	} else {
+		// Use the naive algorithm
+		NaivStandard(A, B, Result, N, N, N)
+	}
 }
 
 func LlamarStrassenNaiv() {
@@ -74,4 +179,20 @@ func LlamarStrassenNaiv() {
 	StrassenNaiv(A, B, Result, N, P, M)
 
 	fmt.Println(Result)
+}
+
+func Plus(A, B, Result [][]float64, N, M int) {
+	for i := 0; i < N; i++ {
+		for j := 0; j < M; j++ {
+			Result[i][j] = A[i][j] + B[i][j]
+		}
+	}
+}
+
+func Minus(A, B, Result [][]float64, N, M int) {
+	for i := 0; i < N; i++ {
+		for j := 0; j < M; j++ {
+			Result[i][j] = A[i][j] - B[i][j]
+		}
+	}
 }
